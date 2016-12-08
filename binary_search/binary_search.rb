2ec21b0f5d7111e13d06d763672ee1ec0042b7b6
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-12-07 23:07:00 -0500

    Returning correct index for 4

--- binary_search/binary_search.rb
+++ binary_search/binary_search.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 def binary_search(obj, arr)
 mid = arr.length/2
-left = [arr[0]..arr[mid -1]]
-right = [arr[mid+1]..arr[-1]]
-
+left = arr[0..arr[(mid-1)]]
+right = arr[(mid+1)..-1]
+# binding.pry
   if obj == arr[mid]
     return mid
   else
@@ -14,8 +16,10 @@ right = [arr[mid+1]..arr[-1]]
   elsif
     obj > arr[mid]
     binary_search(obj,right)
-    
+
   else
     nil
   end
 end
+
+p binary_search(4, [1,2,3,4,5,6,7,8,9,10])
